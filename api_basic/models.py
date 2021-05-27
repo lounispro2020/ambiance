2931d35c9a25f0f5cc8567c95404f0aa2ade11e4
Lounis Hamroun <lounispro2020@gmail.com>
2021-05-28 00:18:49 +0200

    réglage probleme de timestamp base de données

--- api_basic/models.py
+++ api_basic/models.py
@@ -7,6 +7,7 @@ class Temperature(models.Model):
     VALEUR_TEMP = models.CharField(max_length=10)
     #moment = models.CharField(max_length=60)
 
+    Timestamp= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.VALEUR_TEMP
--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -5,5 +5,6 @@ from .models import Temperature
 class TemperatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Temperature
-        fields = ['VALEUR_TEMP']
+        fields = ['VALEUR_TEMP','Timestamp']
+        #fields = ['Timestamp']
    
