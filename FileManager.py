e61b2d0ebc7385cc3e52ca385a0dddd1738d80e7
Lounis Hamroun <lounispro2020@gmail.com>
2021-06-06 21:56:34 +0200

    update

--- FileManager.py
+++ FileManager.py
@@ -28,14 +28,23 @@ length = len(os.listdir('C:/Users/trf_i/OneDrive/Bureau/server/Playlist/'))
 #def run() :
  #    while length > 0: 
  #        playsound ("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
-#music = threading.Thread(target=run)  
+#music = threading.Thread(target=run) 
+#  
+exit_flag = False
 
-
-def run() :
+def run(stop) :
      while length > 0: 
         playsound ("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
+        if exit_flag:
+            break
 
-music = threading.Thread(target=run)
+
+
+
+
+music = threading.Thread(target=run,args=(lambda : exit_flag, ))
+
+
 
 
 global premierefois
@@ -46,9 +55,9 @@ premierefois=True
 
 def main():
     global premierefois
-   
 
     while True :
+        question = input("received y/n: ")
         if input :
             if(premierefois):
                     premierefois=False
@@ -58,6 +67,8 @@ def main():
                       
             else:
                 print("Is thread1 alive:", music.is_alive())
+                exit_flag= True
+                music.join()
                 os.remove("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
                 os.rename('C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique_temp.mp3','C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3')
 
