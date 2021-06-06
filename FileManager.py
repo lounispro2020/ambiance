85dc8305d97753b157ebcf0267c860575d56fa73
lounispro2020 <81303609+lounispro2020@users.noreply.github.com>
2021-06-06 18:18:56 +0200

    Add files via upload
    mise a jout threading musique

--- FileManager.py
+++ FileManager.py
@@ -7,33 +7,47 @@ import time
 from pathlib import Path
 import threading 
 
-#file_name = Path("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique_temp.txt")
-
 
 length = len(os.listdir('C:/Users/trf_i/OneDrive/Bureau/server/Playlist/'))
+
+
+
 
 
 #def rename():
 #   os.rename("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique_temp.mp3", "C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
 
-#if file_name.exists():
-#    print("exists") 
-
-class Music (threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self) :
-        while length > 0: 
-            playsound ("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
 
 
 
-th2 = Music()
 
-th2.start()
 
-th2.join()             
+def run() :
+     while length > 0: 
+         playsound ("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
+music = threading.Thread(target=run)  
+
+premierefois=True
+
+def main():
+
+    while True :
+
+        if raw_input:
+            if(premierefois):
+                    premierefois=False
+                    os.rename('C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique_temp.mp3','C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3') 
+                    music.run()
+                    
+            else:
+                print("Is thread1 alive:", music.is_alive())
+                music.kill()
+                os.remove("C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3")
+                os.rename('C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique_temp.mp3','C:/Users/trf_i/OneDrive/Bureau/server/Playlist/musique.mp3')
+                music.run()
+
+
+main()
 
 
 
