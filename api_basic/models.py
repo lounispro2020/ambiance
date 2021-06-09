2401972268bcce7b16cb95edcbb7d7c0cdf59484
Maxime <maximemerveille59@gmail.com>
2021-06-09 22:07:52 +0200

    Test SQL

--- api_basic/models.py
+++ api_basic/models.py
@@ -12,5 +12,27 @@ class Temperature(models.Model):
     def __str__(self):
         return self.VALEUR_TEMP
 
+class Playlist(models.Model):
+    name = models.CharField(max_length=20)
+
+class Music(models.Model):
+    name = models.CharField(max_length=20)
+    path = models.CharField(max_length=40)
+    Format = models.CharField(max_length=5)
+    artist = models.CharField(max_length=10)
+    playlist = models.ForeignKey('Playlist',on_delete=models.CASCADE)
+
+class Led_color(models.Model):
+    color = models.CharField(max_length = 11)
+
+class Algorithmes(models.Model):
+    begin_time = models.TimeField()
+    end_time = models.TimeField()
+    begin_month = models.DateField()
+    end_month = models.DateField()
+    max_temp = models.DecimalField(max_digits = 2, decimal_places = 0)
+    min_temp = models.DecimalField(max_digits = 2, decimal_places = 0)
+    led = models.ForeignKey('Led_color',on_delete=models.CASCADE)
+    playlist = models.ForeignKey('Playlist',on_delete=models.CASCADE)
 
         
